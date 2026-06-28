27acaa35558f68614c3800593f4cd8e4702eebaf
hostel29 <surajpurprimatricsthostelsuraj@gmail.com>
2026-06-28 23:04:47 +0530

    Update run_server.bat

--- run_server.bat
+++ run_server.bat
@@ -1,4 +1,1 @@
-@echo off
-node server.js
-pause
-npm install helmet cors express-rate-limit xss-clean express-mongo-sanitize pdfkit
+npm install express body-parser multer mongoose cloudinary multer-storage-cloudinary helmet cors express-rate-limit express-mongo-sanitize xss-clean
